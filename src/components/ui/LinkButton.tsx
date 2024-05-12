51c09214fa529ebe3fe9d8f9454972f2cf9b78da
Numberdar4141 <166006607+Motorpedia@users.noreply.github.com>
2024-05-12 22:08:41 +0530

    Update LinkButton.tsx

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -5,7 +5,7 @@ function LinkButton({ text, link }: { text: string; link: string }) {
   return (
     <Link
       to={link}
-      className="flex justify-center max-sm:text-sm items-center gap-3 py-2 md:py-3 px-3 md:px-6 rounded-full bg-green-800 text-white group font-medium"
+      className="flex justify-center max-sm:text-sm items-center gap-3 py-2 md:py-3 px-3 md:px-6 rounded-xl bg-green-800 text-white group font-medium"
     >
       {text}
       <span className="group-hover:rotate-[45deg] duration-500 text-base sm:text-xl lg:text-2xl">
